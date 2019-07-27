5321eaa7af85a27854cdb26c091c5ac63fd75fc8
mdramirez071 <mdramirez071@gmail.com>
2019-07-27 16:42:50 -0700

    updated URL of datasources

--- server/datasources.local.js
+++ server/datasources.local.js
@@ -6,7 +6,7 @@ module.exports = {
   "MongoDB": {
     "host": "",
     "port": 0,
-    "url": "mongodb://localhost:27017",
+    "url": "mongodb://heroku_jt06w17t:a5j04v4t5cjb3c6v3ldrkggnnc@ds255857.mlab.com:55857/heroku_jt06w17t",
     "database": "",
     "password": "",
     "name": "MongoDB",
